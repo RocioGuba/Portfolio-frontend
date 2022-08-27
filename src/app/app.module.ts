2d8b291c473515c2894f59023f26341510dc86a7
RocioGuba <rocioguba2511@gmail.com>
2022-08-27 16:36:40 -0300

    config para celular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgrComponent } from './components/logo-arg-progr/logo-arg-progr.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +20,7 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgProgrComponent,
-    RedesSocialesComponent,
+    
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
