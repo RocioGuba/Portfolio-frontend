c67c06b1395e23a1bcaf8b2aa7bd05af456f8206
RocioGuba <rocioguba2511@gmail.com>
2022-10-04 18:30:51 -0300

    ajustes
    4 de oct

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -1,15 +1,16 @@
 export class persona{
     id?: number;         /*poniendo signo de interrogacion indicamos que ese dato no es necesario aclarar*/
-    nombre: String;
-    apellido: String;
-    img: String;
+    nombre: string;
+    apellido: string;
+    titulo: string;
+    img: string;
 
-    /*son los datos declarados en netbeans*/
-    constructor(nombre: String, apellido: String, img: String){
+    /*son los datos declarados en netbeans(carpeta"persona.java")*/
+    constructor( nombre: string, apellido: string, titulo: string, img: string ){
         this.nombre = nombre;
         this.apellido = apellido;
+        this.titulo = titulo;
         this.img = img;
 
-
-    } 
+    }
 }
